d10440e15cf7a52e3ba14694ef5b190a0043f47f
Administrateur <Administrateur@FORMATI-CLNM8BU>
2018-10-11 16:57:52 +0200

    Version 0.0.1 - Push avancement de la page liste partie Reste à faire (logique de listing des partie et création)

--- public_html/js/liste-partie.js
+++ public_html/js/liste-partie.js
@@ -10,24 +10,26 @@
 $('.creer-une-partie').on("click",createGame);
 
 
-$('.icone-message').on("click","#message",removeMessageFromDOM);
+$(document).on("click",".icone-message",removeMessageFromDOM);
 
-$('.icone-message').on("click","#message",function(){
-    alert("Click icone");
-});
+
 
 function createGame(event){
     event.preventDefault();
     var nomPartie = $("input[name=nomPartie]").val();      
     if(nomPartie.length > 0 ){
-        $("#message").html("<div class='message-succes'>Votre partie : "+nomPartie+" a bien été créée <i class='fas fa-times-circle icone-message'></i></div>").animate({'opacity': 1}, 400);
+        $("#message").html("<div class='message-succes'>Votre partie : "+nomPartie+" a bien été créée <i class='fas fa-times-circle icone-message'></i></div>");
+        $("#message").animate({'opacity': 1, 'height':'show'}, 400);
     }else{
-        $("#message").html("<div class='message-error'>Veuillez renseigner un nom de partie <i class='fas fa-times-circle icone-message'></i></div>").animate({'opacity': 1}, 400);
+        $("#message").html("<div class='message-error'>Veuillez renseigner un nom de partie <i class='fas fa-times-circle icone-message'></i></div>");
+        $("#message").animate({'opacity': 1,' height':'show'}, 400);
     }
 }
 
 function removeMessageFromDOM(){
-    $('#message').html('').slow();
-    $('#message').css('opacity',0);
+    $('#message').animate({'opacity' :0, 'height':'hide'},400);
+    $('[class^="message"]').animate({'opacity' :0, 'height':'hide'},400).html("");
+    
+    
 }
 
